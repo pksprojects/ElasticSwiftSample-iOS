39df1b99edba43907b3108ce650d697b23ece3ef
prafsoni <prafsoni@gmail.com>
2018-03-05 21:34:00 -0600

    Initial Commit

---
+++ ElasticSwiftSample-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ElasticSwiftSample-iOS
+//
+//  Created by Prafull Kumar Soni on 3/5/18.
+//  Copyright © 2018 pksprojects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
