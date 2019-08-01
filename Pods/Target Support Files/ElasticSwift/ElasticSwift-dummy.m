37d5ff53451ab07634dfb84d3cc9d099a0853879
prafsoni <prafsoni@gmail.com>
2019-07-31 19:42:52 -0500

    remove Pods dir

--- Pods/Target Support Files/ElasticSwift/ElasticSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ElasticSwift : NSObject
-@end
-@implementation PodsDummy_ElasticSwift
-@end
--- Pods/Target Support Files/Pods-ElasticSwiftSample-iOSTests/Pods-ElasticSwiftSample-iOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ElasticSwiftSample_iOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ElasticSwiftSample_iOSTests
-@end
